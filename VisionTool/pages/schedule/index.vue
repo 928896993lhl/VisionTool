03d1426359fc0748ac11f7e6c1f2edc4cf95acad
lhl <928896993@qq.com>
2024-08-16 13:31:06 +0800

    修改日程页，搭建初步页面框架，设置tabbar图标

--- VisionTool/pages/schedule/index.vue
+++ VisionTool/pages/schedule/index.vue
@@ -1,113 +1,60 @@
 <template>
-
-	<view class="content">
-		<uni-swiper-dot class="uni-swiper-dot-box" @clickItem=clickItem :info="info" :current="current" :mode="mode"
-			:dots-styles="dotsStyles" field="content">
-			<swiper class="swiper-box" @change="change" :current="swiperDotIndex">
-				<swiper-item v-for="(item, index) in 3" :key="index">
-					<view class="swiper-item" :class="'swiper-item' + index">
-						<text style="color: #fff; font-size: 32px;">{{index+1}}</text>
-					</view>
-				</swiper-item>
-			</swiper>
-		</uni-swiper-dot>
+	<view>
+		<swiper class="swiper-box" :indicator-dots="true" :autoplay="true" @change="swiperchange">
+			<swiper-item v-for="(item ,index) in swiperinfo" :key="index">
+				<view class="swiper-item">
+					<image :mode="aspectFit" :src="item.url"></image>
+				</view>
+			</swiper-item>
+		</swiper>
 	</view>
-
-	<view class="container" v-if="showCalendar">
+	<view class="container">
 		<uni-section title="日程表" type="line"></uni-section>
 		<view>
-			<!-- 插入模式 -->
-			<uni-calendar class="uni-calendar--hook" :selected="info.selected" :showMonth="false" @change="change"
+			<uni-calendar class="uni-calendar--hook" :selected="info.selected" :showMonth="true" @change="change"
 				@monthSwitch="monthSwitch" />
 		</view>
 	</view>
 </template>
 
 <script>
-	/**
-	 * 获取任意时间
-	 */
-	function getDate(date, AddDayCount = 0) {
-		if (!date) {
-			date = new Date();
-		}
-		if (typeof date !== 'object') {
-			date = date.replace(/-/g, '/');
-		}
-		const dd = new Date(date);
-
-		dd.setDate(dd.getDate() + AddDayCount); // 获取AddDayCount天后的日期
-
-		const y = dd.getFullYear();
-		const m = dd.getMonth() + 1 < 10 ? '0' + (dd.getMonth() + 1) : dd.getMonth() + 1; // 获取当前月份的日期，不足10补0
-		const d = dd.getDate() < 10 ? '0' + dd.getDate() : dd.getDate(); // 获取当前几号，不足10补0
-		return {
-			fullDate: y + '-' + m + '-' + d,
-			year: y,
-			month: m,
-			date: d,
-			day: dd.getDay()
-		};
-	}
 	export default {
 		components: {},
 		data() {
 			return {
-				info: [{
-						colorClass: 'uni-bg-red',
+				swiperinfo: [{
 						url: 'https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/shuijiao.jpg',
-						content: '内容 A'
 					},
 					{
-						colorClass: 'uni-bg-green',
 						url: 'https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/shuijiao.jpg',
-						content: '内容 B'
 					},
 					{
-						colorClass: 'uni-bg-blue',
 						url: 'https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/shuijiao.jpg',
-						content: '内容 C'
+					},
+					{
+						url: 'https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/shuijiao.jpg',
 					}
 				],
-				modeIndex: -1,
-				styleIndex: -1,
-				current: 0,
-				mode: 'default',
-				dotsStyles: {},
-				swiperDotIndex: 0,
-				showCalendar: false,
 				info: {
-					lunar: true,
-					range: true,
-					insert: false,
-					selected: []
+					selected: [{
+						date: '2024-08-27',
+						info: '已预约',
+						data: {
+							custom: '自定义信息',
+							name: '自定义消息头',
+						}
+					}]
 				}
 			};
 		},
-		onReady() {
-			this.$nextTick(() => {
-				this.showCalendar = true;
-			});
-			// TODO 模拟请求异步同步数据
-			setTimeout(() => {
-				this.info.date = getDate(new Date(), -30).fullDate;
-				this.info.startDate = getDate(new Date(), -60).fullDate;
-				this.info.endDate = getDate(new Date(), 30).fullDate;
-				this.info.selected = [];
-			}, 2000);
-		},
+		onReady() {},
 		methods: {
+			swiperchange(e) {
+				this.current = e.detail.current;
+				console.log('change 返回:', e.detail.current);
+			},
 			change(e) {
 				console.log('change 返回:', e);
-				// 模拟动态打卡
-				if (this.info.selected.length > 5) return;
-				this.info.selected.push({
-					date: e.fulldate,
-					info: '打卡'
-				});
-			},
-			confirm(e) {
-				console.log('confirm 返回:', e);
 			},
 			monthSwitch(e) {
 				console.log('monthSwitchs 返回:', e);
@@ -128,5 +75,20 @@
 		flex: 1;
 		font-weight: bold;
 		font-size: 32rpx;
+	}
+
+	.swiper-box {
+		height: 200px;
+	}
+
+	.swiper-item {
+		/* #ifndef APP-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 200px;
+		color: #fff;
 	}
 </style>
